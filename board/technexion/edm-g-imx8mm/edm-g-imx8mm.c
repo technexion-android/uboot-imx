ce10824a9e73116a0842b5213b9ebf9a051f86aa
Tifa Chen <tifa.chen@technexion.com>
2024-06-04 01:01:10 -0700

    edm-g-imx8mm: Enable camera and panel auto detection

--- board/technexion/edm-g-imx8mm/edm-g-imx8mm.c
+++ board/technexion/edm-g-imx8mm/edm-g-imx8mm.c
@@ -414,12 +414,10 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 
 int board_late_init(void)
 {
-#ifndef CONFIG_AVB_SUPPORT
 	detect_baseboard();
 #ifdef CONFIG_TN_PHERIPHERAL_DETECT
 	detect_display_panel();
 	detect_camera();
-#endif
 #endif
 
 #ifdef CONFIG_ENV_IS_IN_MMC
