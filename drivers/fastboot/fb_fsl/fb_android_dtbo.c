63f1af4e4e1e2e5cca267c8fc4db74bfc87fdc69
Tifa Chen <tifa.chen@technexion.com>
2025-02-21 00:46:28 -0800

    fb_fsl: Update edm-g-imx8mm dtbo support
    Based on kernel 6.6.36

--- drivers/fastboot/fb_fsl/fb_android_dtbo.c
+++ drivers/fastboot/fb_fsl/fb_android_dtbo.c
@@ -72,10 +72,7 @@ static dev_setup_t _dev_setup_edm_g_8mm[] = {
 	{ DEV_SETUP_VOUT, "sn65dsi84-vl10112880", NULL },
 	{ DEV_SETUP_VOUT, "sn65dsi84-vl15613676", NULL },
 	{ DEV_SETUP_VOUT, "sn65dsi84-vl215192108", NULL },
-	{ DEV_SETUP_VIN, "tevi-ov5640", NULL },
-	{ DEV_SETUP_VIN, "tevi-ap1302", "ar0144" },
 	{ DEV_SETUP_VIN, "hdmi2mipi-tc358743", NULL },
-	{ DEV_SETUP_VIN, "vizionlink-tevi-ap1302", "ar0144" },
 	{ DEV_SETUP_VIN, "tevs", "ar0144" },
 	{ DEV_SETUP_VIN, "vls", "ar0144" },
 };
