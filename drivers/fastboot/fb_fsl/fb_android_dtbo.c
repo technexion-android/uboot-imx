7fe4bf9d5fcc8a7d71a794d3dd1d47b97066e139
Tifa Chen <tifa.chen@technexion.com>
2025-10-08 02:33:07 -0700

    fb_fsl: Update edm-g-imx8mp dtbo support for match kernel 6.12.23

--- drivers/fastboot/fb_fsl/fb_android_dtbo.c
+++ drivers/fastboot/fb_fsl/fb_android_dtbo.c
@@ -31,14 +31,12 @@ static dev_setup_t _dev_setup_axon_8mp[] = {
 static dev_setup_t _dev_setup_edm_g_8mp[] = {
 	{ DEV_SETUP_VOUT, "lvds-vl10112880", NULL },
 	{ DEV_SETUP_VOUT, "lvds-vl15613676", NULL },
-	{ DEV_SETUP_VOUT, "lvds-vl215192108", NULL },
+	{ DEV_SETUP_VOUT, "lvds-vl156192108", NULL },
 	{ DEV_SETUP_VIN, "tevs", NULL },
 	{ DEV_SETUP_VIN, "vls", NULL },
 };
 static dev_setup_t _dev_setup_edm_g_8mp_wizard[] = {
 	{ DEV_SETUP_VOUT, "vizionpanel-vl10112880", NULL },
-	{ DEV_SETUP_VOUT, "vizionpanel-vl15010276", NULL },
-	{ DEV_SETUP_VOUT, "vizionpanel-vl15613676", NULL },
 	{ DEV_SETUP_VOUT, "mipi2hdmi-adv7535", NULL },
 	{ DEV_SETUP_VOUT, "ili9881c", NULL },
 };
